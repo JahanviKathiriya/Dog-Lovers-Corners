3919cf17ff8984fc12a9868b2774e1cc8492f4a5
jahanvikathiriya <jckathiriya@crimson.ua.edu>
2023-10-08 15:09:24 -0500

    transfering to visual studios

--- app/Dog-Lovers-Corner/dog-lovers-corner/src/main.jsx
+++ app/Dog-Lovers-Corner/dog-lovers-corner/src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.css"
 import Carousel from './component/carousel';
 import DogBreeds from './component/breedsList';
 import NavbarNav from './component/navbar';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,8 +14,3 @@ root.render(
     <Carousel />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
